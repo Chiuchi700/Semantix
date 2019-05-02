a8213332e22e94c8bf2cb1b36303022874703514
Chiuchi700 <caiochiuchi3@gmail.com>
2019-05-02 15:20:49 -0300

    Crawlers

---
+++ cripto.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+from datetime import datetime
+import requests
+import csv
+import os
+
+link = requests.get(url="https://m.investing.com/crypto/", headers={'User-Agent':'curl/7.52.1'})
+
+now = datetime.now()
+time = datetime.timestamp(now)
+
+soup = BeautifulSoup(link.content, 'html.parser')
+
+s = soup.find_all('tr')
+	
+text = []
+saida = []
+
+#realiza a captura das moedas e armazena em uma string saida
+for i in s[1:]:
+    #realiza substituição dos tabs por espaço vazio e separa moedas por linha
+    text = i.getText().replace('\t', "").split("\n")
+
+    pos = list(filter(None,text))
+    #print(pos)
+
+    saida = csv.writer(open('criptomoedas.csv','a+'), delimiter=',')
+    if os.path.getsize('criptomoedas.csv') == 0:
+        saida.writerow(['code', 'name', 'priceUSD', 'change24H', 'change7D', 'symbol', 'priceBTC', 'marketCap', 'volume24H', 'totalVolume', 'timestamp'])
+
+        saida.writerow([pos[0],pos[1],pos[2],pos[3],pos[4],pos[5],pos[6],pos[7],pos[8],pos[9],time])
+    else:
+        saida.writerow([pos[0],pos[1],pos[2],pos[3],pos[4],pos[5],pos[6],pos[7],pos[8],pos[9],time])
+
